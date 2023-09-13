5bca97923c3014093ce57bf0b61b2869acd3c504
William Alonso <williamalonso@outlook.com>
2023-09-13 19:08:59 -0300

    starting app

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,1 +1,14 @@
 import React from 'react';
+
+const Login: React.FC = () => {
+
+  function handleLogin() {}
+  
+  return(
+    <>
+      <button onClick={handleLogin}></button>
+    </>
+  );
+}
+
+export default Login;
