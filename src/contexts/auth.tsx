fbb1cc0ec662efe060f287f38e7f23322283d52b
William Alonso <williamalonso@outlook.com>
2023-09-14 15:48:13 -0300

    :zap: implementig login redirection

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,23 +1,54 @@
 import React, {createContext, ReactNode} from 'react';
+import api from '../services/api';
 
 // Aqui estamos definindo a forma dos dados que serão fornecidos pelo Contexto
 interface AuthContextData {
   signed: boolean;
+  Login(
+    email:string,
+    password:string,
+    navigate:()=>void
+  ): Promise<void>;
 }
 
-const AuthContext = createContext<AuthContextData | undefined>(undefined);
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 interface AuthProviderProps {
   children: ReactNode;
 }
 
-// Dentro do componente AuthProvider, você usa <AuthContext.Provider> para fornecer o contexto aos componentes filhos. Neste caso, você está definindo o valor do contexto como { signed: true }, indicando que o usuário está autenticado. Isso é apenas um exemplo; você pode substituir esse valor real com base na lógica de autenticação de sua aplicação.
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   return(
-    <AuthContext.Provider value={{ signed:true }}>
+    <AuthContext.Provider value={{ signed: true, Login }}>
       {children}
     </AuthContext.Provider>
   );
+}
+
+// chamar a api
+async function Login(email: string, password: string, navigate: () => void) {
+  try {
+
+    const fakeData = {
+      data: {
+        token: 'DHFDHUDFH-DFDFJDH',
+        user: {
+          email: email,
+          password: password,
+        }
+      }
+    }
+
+    await new Promise( (resolve) => setTimeout(resolve, 1000) );
+
+    console.log('login bem sucedido:', fakeData);
+
+    navigate();
+
+    
+  } catch(e) {
+    console.error('Erro ao fazer login: ', e);
+  }
 }
 
 export default AuthContext;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,47 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import AuthContext from '../contexts/auth';
+import { useNavigate } from 'react-router-dom';
 
 const Login: React.FC = () => {
 
   const context = useContext(AuthContext);
-  console.log(context);
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const navigate = useNavigate();
 
-  function handleLogin() {}
+  async function handleLogin() {
+    try {
+      await context.Login(email, password, () => {
+        navigate('/home');
+      });
+    } catch(e) {
+      console.error('Erro ao fazer login bro: ', e);
+    }
+  }
   
   return(
-    <>
-      <button onClick={handleLogin}>login</button>
-    </>
+    <div className="container mx-auto flex flex-col w-40">
+      <input
+        type="text"
+        placeholder="E-mail"
+        value={email.toString()}
+        onChange={(e) => setEmail(e.target.value)}
+        className="mt-5"
+      />
+      <input
+        type="password"
+        placeholder="Senha"
+        value={password.toString()}
+        onChange={(e) => setPassword(e.target.value)}
+        className="mt-5"
+      />
+      <button 
+        onClick={handleLogin}
+        className="mt-5 border-double border-4 border-sky-500"
+      >
+        login
+      </button>
+    </div>
   );
 }
 
